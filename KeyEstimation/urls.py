18cffa9b5506ee61a24b871587b15e16993dd427
Dylan Martinez <dylanmartinez@Dylans-MacBook-Air.local>
2017-10-13 13:17:59 -0600

    now using the include function and creating a urls file in our application folder

--- KeyEstimation/urls.py
+++ KeyEstimation/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from WebApp import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'^admin/', admin.site.urls)
+    url(r'^WebApp/',include('WebApp.urls')),
 ]
 
 
