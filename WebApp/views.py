9d126287f5c25f0878a18d72080c390283c43ba3
Dylan Martinez <dylanmartinez@Dylans-MacBook-Air.local>
2017-10-13 12:38:45 -0600

    now using html code

--- WebApp/views.py
+++ WebApp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("<i> Hello World </i")
 
 # Create your views here.
