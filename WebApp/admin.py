7df16a052ef13c92647cca1ed6e6161b6ce44d9d
Parker Watkins <parkergw@gmail.com>
2017-11-10 11:30:07 -0700

    registered models in admin.py

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from WebApp.models import UserProfileInfo
+from WebApp.models import *
 # Register your models here.
 
 admin.site.register(UserProfileInfo)
+admin.site.register(openBudgets)
+admin.site.register(pendingBudgets)
+admin.site.register(awardedBudgets)
+admin.site.register(closedBudgets)
+admin.site.register(castInPlace)
+admin.site.register(footings)
+admin.site.register(slabOnGrade)
+admin.site.register(tiltUp)
+admin.site.register(mixDesign)
