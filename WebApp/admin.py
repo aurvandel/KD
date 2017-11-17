49c79572b329d90d477a939f3fa7c0b751c75060
Parker Watkins <parkergw@gmail.com>
2017-11-17 14:11:43 -0700

    update

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -4,8 +4,8 @@ from WebApp.models import *
 
 admin.site.register(UserProfileInfo)
 admin.site.register(openBudgets)
-admin.site.register(openBudgets.pendingBudgets.awardedBudgets.closedBudgets.castInPlace)
-admin.site.register(openBudgets.pendingBudgets.awardedBudgets.closedBudgets.castInPlace.footings)
-admin.site.register(openBudgets.pendingBudgets.awardedBudgets.closedBudgets.castInPlace.footings.slabOnGrade)
-admin.site.register(openBudgets.pendingBudgets.awardedBudgets.closedBudgets.castInPlace.footings.slabOnGrade.tiltUp)
-admin.site.register(openBudgets.pendingBudgets.awardedBudgets.closedBudgets.castInPlace.footings.slabOnGrade.tiltUp.mixDesign)
+admin.site.register(openBudgets.castInPlace)
+admin.site.register(openBudgets.castInPlace.footings)
+admin.site.register(openBudgets.castInPlace.footings.slabOnGrade)
+admin.site.register(openBudgets.castInPlace.footings.slabOnGrade.tiltUp)
+admin.site.register(openBudgets.castInPlace.footings.slabOnGrade.tiltUp.mixDesign)
