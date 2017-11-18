8fd49ba36d70b4f76e9676c4ec47ea953b00fa8f
brockmorrison92 <brock.morrison92@gmail.com>
2017-11-18 14:59:30 -0700

    garbage commit... new commit immediately following

--- WebApp/models.py
+++ WebApp/models.py
@@ -104,66 +104,66 @@ class openBudgets(models.Model): #come back in and set appropriate fields to bla
             ordering = ['id']
 
 
-        class footings(models.Model):
+    class footings(models.Model):
+        # id = models.AutoField(primary_key=True)       id field is handled by django
+        footing_description = models.CharField(max_length = 255, unique = False)
+        footing_type = models.CharField(max_length = 255, unique = False)
+        footing_width = models.PositiveSmallIntegerField() #width in inches
+        footing_depth = models.PositiveSmallIntegerField() #width in inches
+        footing_length = models.PositiveSmallIntegerField() #width in feet
+        footing_area = models.DecimalField(max_digits = 15, decimal_places = 4)
+        footing_concrete_mix = models.CharField(max_length = 255, unique = False) #will eventually be a selection list populated from another table
+        footing_concrete_grout = models.CharField(max_length = 255, unique = False) #will eventually be a selection list populated from another table
+        footing_lumber = models.CharField(max_length = 255, unique = False) #will eventually be a selection list populated by another table
+        footing_misc_materials = models.CharField(max_length = 255, unique = False) #will eventually be selection list populated by another table
+
+    class slabOnGrade(models.Model):
+        # id = models.AutoField(primary_key=True)       id field is handled by django
+        sog_grading_material_thickness = models.PositiveSmallIntegerField() #thickness in inches
+        sog_thickness = models.PositiveSmallIntegerField() #thickness measured in inches
+        sog_width = models.PositiveSmallIntegerField() #measured in feet
+        sog_length = models.PositiveSmallIntegerField() #measured in feet
+        sog_area = models.DecimalField(max_digits = 15, decimal_places = 4)
+        sog_perimeter = models.DecimalField(max_digits = 15, decimal_places = 4)
+        sog_concrete_mix = models.CharField(max_length = 60, unique = False) #will eventually be a selection list populated by another table
+        sog_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
+        sog_second_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
+        sog_cure = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
+        sog_perimeter_forms = models.CharField(max_length = 100, unique = False) #Need to check to see if this is a length or a description
+        sog_bulk_heads = models.CharField(max_length = 100, unique = False) #need to check to see if this is a number or a description
+        sog_vapor_barrier = models.CharField(max_length = 100, unique = False) #need to check to see if this is a number or a description
+        sog_grading_material = models.CharField(max_length = 100, unique = False) #type of grading material... might be replaced with a list populated from another table
+        sog_anchor_bolts = models.CharField(max_length = 100, unique = False) #need to check to see if this is going to be a selection from a list or a description
+        #sog_concrete_misc_material #check to see what this is
+        #sog_material_misc_material
+        sog_subcontractor_first = models.CharField(max_length = 100, unique = False) #probably needs to be populated from another table and be a selection list
+        sog_subcontractor_second = models.CharField(max_length = 100, unique = False)#probably needs to be populated from another table and be a selection list
+        sog_subcontractor_third = models.CharField(max_length = 100, unique = False)#probably needs to be populated from another table and be a selection list
+
+        class tiltUp(models.Model):
             # id = models.AutoField(primary_key=True)       id field is handled by django
-            footing_description = models.CharField(max_length = 255, unique = False)
-            footing_type = models.CharField(max_length = 255, unique = False)
-            footing_width = models.PositiveSmallIntegerField() #width in inches
-            footing_depth = models.PositiveSmallIntegerField() #width in inches
-            footing_length = models.PositiveSmallIntegerField() #width in feet
-            footing_area = models.DecimalField(max_digits = 15, decimal_places = 4)
-            footing_concrete_mix = models.CharField(max_length = 255, unique = False) #will eventually be a selection list populated from another table
-            footing_concrete_grout = models.CharField(max_length = 255, unique = False) #will eventually be a selection list populated from another table
-            footing_lumber = models.CharField(max_length = 255, unique = False) #will eventually be a selection list populated by another table
-            footing_misc_materials = models.CharField(max_length = 255, unique = False) #will eventually be selection list populated by another table
-
-            class slabOnGrade(models.Model):
-                # id = models.AutoField(primary_key=True)       id field is handled by django
-                sog_grading_material_thickness = models.PositiveSmallIntegerField() #thickness in inches
-                sog_thickness = models.PositiveSmallIntegerField() #thickness measured in inches
-                sog_width = models.PositiveSmallIntegerField() #measured in feet
-                sog_length = models.PositiveSmallIntegerField() #measured in feet
-                sog_area = models.DecimalField(max_digits = 15, decimal_places = 4)
-                sog_perimeter = models.DecimalField(max_digits = 15, decimal_places = 4)
-                sog_concrete_mix = models.CharField(max_length = 60, unique = False) #will eventually be a selection list populated by another table
-                sog_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
-                sog_second_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
-                sog_cure = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
-                sog_perimeter_forms = models.CharField(max_length = 100, unique = False) #Need to check to see if this is a length or a description
-                sog_bulk_heads = models.CharField(max_length = 100, unique = False) #need to check to see if this is a number or a description
-                sog_vapor_barrier = models.CharField(max_length = 100, unique = False) #need to check to see if this is a number or a description
-                sog_grading_material = models.CharField(max_length = 100, unique = False) #type of grading material... might be replaced with a list populated from another table
-                sog_anchor_bolts = models.CharField(max_length = 100, unique = False) #need to check to see if this is going to be a selection from a list or a description
-                #sog_concrete_misc_material #check to see what this is
-                #sog_material_misc_material
-                sog_subcontractor_first = models.CharField(max_length = 100, unique = False) #probably needs to be populated from another table and be a selection list
-                sog_subcontractor_second = models.CharField(max_length = 100, unique = False)#probably needs to be populated from another table and be a selection list
-                sog_subcontractor_third = models.CharField(max_length = 100, unique = False)#probably needs to be populated from another table and be a selection list
-
-                class tiltUp(models.Model):
-                    # id = models.AutoField(primary_key=True)       id field is handled by django
-                    tilt_up_panel_description = models.CharField(max_length = 255, unique = False)
-                    tilt_up_panel_thickness = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in inches
-                    tilt_up_panel_width = models.DecimalField(max_digits = 15, decimal_places = 4) #measred in feet
-                    tilt_up_panel_height = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in feet
-                    tilt_up_panel_area = models.DecimalField(max_digits = 15, decimal_places = 4)
-                    tilt_up_forms = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in linear feet
-                    tilt_up_perimeter = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in linear feet
-                    tilt_up_gross_sqft = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in sq/ft
-                    tilt_up_openings_sqft = models.DecimalField(max_digits = 15, decimal_places = 4)
-                    tilt_up_net_sqft = models.DecimalField(max_digits = 15, decimal_places = 4)
-                    tilt_up_panel_weight = models.PositiveSmallIntegerField() #measured in tons
-                    tilt_up_panel_concrete_mix = models.CharField(max_length = 150, unique = False)
-                    tilt_up_panel_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
-                    tilt_up_panel_second_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
-                    tilt_up_panel_forms = models.CharField(max_length = 100, unique = False)
-                    tilt_up_panel_engineering = models.CharField(max_length = 100, unique = False)
-                    tilt_up_panel_crane = models.CharField(max_length = 100, unique = False)
-                    tilt_up_panel_caulking = models.CharField(max_length = 100, unique = False)
-                    tilt_up_panel_pumping = models.CharField(max_length = 100, unique = False)
-                    tilt_up_panel_welding = models.CharField(max_length = 100, unique = False)
-                    tilt_up_sack_patch = models.CharField(max_length = 100, unique = False)
-                    tilt_up_brace_rental = models.CharField(max_length = 100, unique = False)
+            tilt_up_panel_description = models.CharField(max_length = 255, unique = False)
+            tilt_up_panel_thickness = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in inches
+            tilt_up_panel_width = models.DecimalField(max_digits = 15, decimal_places = 4) #measred in feet
+            tilt_up_panel_height = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in feet
+            tilt_up_panel_area = models.DecimalField(max_digits = 15, decimal_places = 4)
+            tilt_up_forms = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in linear feet
+            tilt_up_perimeter = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in linear feet
+            tilt_up_gross_sqft = models.DecimalField(max_digits = 15, decimal_places = 4) #measured in sq/ft
+            tilt_up_openings_sqft = models.DecimalField(max_digits = 15, decimal_places = 4)
+            tilt_up_net_sqft = models.DecimalField(max_digits = 15, decimal_places = 4)
+            tilt_up_panel_weight = models.PositiveSmallIntegerField() #measured in tons
+            tilt_up_panel_concrete_mix = models.CharField(max_length = 150, unique = False)
+            tilt_up_panel_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
+            tilt_up_panel_second_additive = models.CharField(max_length = 100, unique = False) #will eventually be a selection list populated by another table
+            tilt_up_panel_forms = models.CharField(max_length = 100, unique = False)
+            tilt_up_panel_engineering = models.CharField(max_length = 100, unique = False)
+            tilt_up_panel_crane = models.CharField(max_length = 100, unique = False)
+            tilt_up_panel_caulking = models.CharField(max_length = 100, unique = False)
+            tilt_up_panel_pumping = models.CharField(max_length = 100, unique = False)
+            tilt_up_panel_welding = models.CharField(max_length = 100, unique = False)
+            tilt_up_sack_patch = models.CharField(max_length = 100, unique = False)
+            tilt_up_brace_rental = models.CharField(max_length = 100, unique = False)
 
 class mixDesign(models.Model):
     # id = models.AutoField(primary_key=True)       id field is handled by django
