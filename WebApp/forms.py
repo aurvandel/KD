0b38537b3a1f9bc6ef73b2d1a3e80577f246d861
Parker Watkins <parkergw@gmail.com>
2017-11-20 10:36:13 -0700

    one step closer to correcting the formatting for footings form

--- WebApp/forms.py
+++ WebApp/forms.py
@@ -8,6 +8,57 @@ class NewFootingForm(forms.ModelForm):
         model = openBudgets.footings
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        super(NewFootingForm, self).__init__(*args, **kwargs)
+        self.fields['footing_description'].widget.attrs={
+            'id': 'footing_description',
+            'class': 'form-control',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_type'].widget.attrs={
+            'id': 'footing_description',
+            'class': 'form-control',
+            'aria-describedby': 'Text input with segmented button dropdown'
+        }
+        self.fields['footing_width'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_depth'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_length'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_area'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_concrete_mix'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_concrete_grout'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
+        self.fields['footing_lumber'].widget.attrs={
+            'class': 'form-control',
+            'aria-label': 'Text input with segmented button dropdown'
+        }
+        self.fields['footing_misc_materials'].widget.attrs={
+            'class': 'form-control',
+            'id': 'footing_width',
+            'aria-describedby': 'basic-addon3'
+        }
 
 # class UserForm(forms.ModelForm):
 #     password = forms.CharField(widget=forms.PasswordInput())
